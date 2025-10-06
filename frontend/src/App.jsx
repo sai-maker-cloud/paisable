91b4c49c38599eb602b4bebc8146650cb5624b17
pranjal.arora <pranjal.arora@officebanao.com>
2025-10-06 21:41:20 +0530

    formatting fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import SetupPage from "./pages/SetupPage";
-import DashboardPage from "./pages/DashboardPage";
-import { TransactionsPage } from "./pages/TransactionsPage";
-import ReceiptsPage from "./pages/ReceiptsPage";
-import WelcomePage from "./pages/WelcomePage";
-import ProtectedRoute from "./components/ProtectedRoute";
-import SetupProtectedRoute from "./components/SetupProtectedRoute";
-import Layout from "./components/Layout";
-import SettingsPage from "./pages/SettingsPage";
-import RecurringTransactions from "./pages/RecurringTransactions";
-import ContactUs from "./pages/ContactUs";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import SetupPage from './pages/SetupPage';
+import DashboardPage from './pages/DashboardPage';
+import { TransactionsPage } from './pages/TransactionsPage';
+import ReceiptsPage from './pages/ReceiptsPage';
+import WelcomePage from './pages/WelcomePage';
+import ProtectedRoute from './components/ProtectedRoute';
+import SetupProtectedRoute from './components/SetupProtectedRoute';
+import Layout from './components/Layout';
+import SettingsPage from './pages/SettingsPage';
+import RecurringTransactions from './pages/RecurringTransactions';
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
