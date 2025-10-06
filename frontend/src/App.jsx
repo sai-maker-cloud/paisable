cbffe2b55543af182803095b1605a77d7e3fc862
archa8 <archa.ug22.me@nitp.ac.in>
2025-10-06 21:53:08 +0530

    fix: merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,39 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import DashboardPage from "./pages/DashboardPage";
-import {  TransactionsPage  } from "./pages/TransactionsPage";
-import ReceiptsPage from "./pages/ReceiptsPage";
-import WelcomePage from "./pages/WelcomePage";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Layout from "./components/Layout";
-import Budgets from "./pages/Budgets";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WelcomePage from './pages/WelcomePage';
+import DashboardPage from './pages/DashboardPage';
+import { TransactionsPage } from './pages/TransactionsPage';
+import ReceiptsPage from './pages/ReceiptsPage';
 import SettingsPage from './pages/SettingsPage';
+import Budgets from './pages/Budgets';
+import SetupPage from './pages/SetupPage';
+import ContactUs from './pages/ContactUs';
+import Layout from './components/Layout';
+import ProtectedRoute from './components/ProtectedRoute';
+import SetupProtectedRoute from './components/SetupProtectedRoute';
 
 function App() {
   return (
     <Routes>
-      {/* Public Routes */}
       <Route path="/" element={<WelcomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-
-      {/* Protected Routes Wrapper */}
+      <Route path="/contact" element={<ContactUs />} />
       <Route
+        path="/setup"
         element={
           <ProtectedRoute>
-            <Layout />
+            <SetupPage />
           </ProtectedRoute>
+        }
+      />
+      <Route
+        element={
+          <SetupProtectedRoute>
+            <Layout />
+          </SetupProtectedRoute>
         }
       >
         <Route path="/dashboard" element={<DashboardPage />} />
