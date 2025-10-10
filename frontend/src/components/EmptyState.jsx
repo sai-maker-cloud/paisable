edfb7e70944bed488ea691b44fee8708a0ccf33e
Aditya <adityarocks123456@gmail.com>
2025-10-10 21:36:37 +0530

    Fixed the layout and color scheme of light mode for dashboard div's

--- frontend/src/components/EmptyState.jsx
+++ frontend/src/components/EmptyState.jsx
@@ -4,14 +4,13 @@ export default function EmptyState({ message , icon }) {
   return (
     <div className="flex justify-center items-center w-full h-full">
       <div className="relative max-w-sm w-full">
-        <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-gray-800 rounded-xl"></span>
-        <div className="relative h-full p-6 bg-gray-900 border border-gray-700 rounded-xl shadow-lg flex flex-col items-center justify-center text-center">
+        <div className="relative h-full p-6 bg-blue-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-xl shadow-lg flex flex-col items-center justify-center text-center transition-colors duration-300">
           {icon && (
-            <div className="flex items-center justify-center w-14 h-14 mb-3 bg-gray-800 rounded-lg">
+            <div className="flex items-center justify-center w-14 h-14 mb-3 bg-blue-300 dark:bg-gray-800 rounded-lg">
               {icon}
             </div>
           )}
-          <h3 className="text-lg font-semibold text-gray-100">
+          <h3 className="text-lg font-semibold text-black dark:text-gray-100">
             {message}
           </h3>
         </div>
